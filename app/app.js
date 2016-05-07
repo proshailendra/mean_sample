e0df245cdb66d2cfdab062b523d0d88f8e96e785
Shailendra Chauhan <pro.shailendra@gmail.com>
2016-05-07 18:26:53 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -21,5 +21,5 @@ angular.module("app", ["ui.router"])
         });
     })
     .constant("globalConfig", {
-        apiAddress: 'http://localhost:3000/api/userservice'
+        apiAddress: 'http://dotnettricks.herokuapp.com/api/userservice'
     });
